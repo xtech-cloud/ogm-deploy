cbd096a6f749d06057639507099f38c6bae83c00
Easlee Liu <easlee@outlook.com>
2021-07-23 01:57:46 +0800

    update

--- ogm-sandbox/.startup.sh
+++ ogm-sandbox/.startup.sh
@@ -1,2 +1,2 @@
 pkill ogm-startkit
-MSA_CONFIG_DEFINE='{"source":"file","prefix":"/ogm/conf/","key":"startkit.yaml"}' /ogm/bin/ogm-startkit &
+MSA_CONFIG_DEFINE='{"source":"file","prefix":"/ogm/msa/conf","key":"startkit.yaml"}' /ogm/msa/bin/ogm-startkit &
